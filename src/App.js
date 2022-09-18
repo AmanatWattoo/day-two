e7a2d673bcf23425465e953fb76aedf489bc7baa
AmanatWattoo <amanatwattoo14825@gmail.com>
2022-09-18 15:30:16 +0500

    js

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
             <input type="text" placeholder='Enter your name here' maxLength={12}></input>
             <input type="Password" placeholder='Enter your Password here' maxLength={12}></input>
             <input type="Password" placeholder='Type again password' maxLength={12}></input>
+            <input type="Date">Date</input>
             <input type="submit" value="submit">submit</input>
           </form>
         </fieldset>
